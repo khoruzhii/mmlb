391d1a78515f49c5cc49ce43206c130e38c3f08f
Jakob Moosbauer <jakob.moosbauer@gmail.com>
2026-08-03 14:27:39 +0200

    initial commit on rewrite

---
+++ src/tensor.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "types.h"
+
+class Tensor {
+  private:
+    std::array<U64,64> data;
+  public:
+    bool get_bit(size_t i, size_t j, size_t k) const {
+        return (data[4*i + j/4] >> (k + 16*(j%4))) & U64(1);
+    }
+
+    bool operator[](size_t i, size_t j, size_t k) const {
+        return get_bit(i, j, k);
+    }
+
+    U_16 operator[](size_t i, size_t j) const {
+        return (data[4*i + j/4] >> (16*(j%4))) & U64(0xFFFF);
+    }
+
